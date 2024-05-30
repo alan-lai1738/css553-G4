ea277494b135a6f2ba834732616afac4a1e80d4e
PamelaCheema <pamelacheema@live.com>
2024-05-29 18:23:57 -0700

    added readFile

--- helloworldtest.js
+++ helloworldtest.js
@@ -3,21 +3,8 @@ const PDFParser = require('pdf2json');
 
 const pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", (errData) => 
-    console.error(errData.parserError)
-);
-pdfParser.on("pdfParser_dataReady", (pdfData) => {
-    fs.writeFile(
-        "test.json",
-        JSON.stringify(pdfData),
-        (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('File successfully written!');
-            }
-        }
-    );
-});
 
-pdfParser.loadPDF("test.pdf");
+var linkedinPdfToJson = new LinkedInPdfToJson();
+const inputFilePath = fileName;
+const outputFilePath = "output/data.json";
+const data = linkedinPdfToJson.run(inputFilePath, outputFilePath);
